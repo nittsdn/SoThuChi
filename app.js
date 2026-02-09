78f0dfe2d0473292cfda2f697c3942cabbee7711
Moon-gazing Cat <nittsdn@gmail.com>
2026-02-09 14:58:16 +0700

    1457

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Version: v2.4.1450
+// Version: v2.4.1457
 // ================= CONSTANTS =================
 const API_URL = "https://script.google.com/macros/s/AKfycbzjor1H_-TcN6hDtV2_P4yhSyi46zpoHZsy2WIaT-hJfoZbC0ircbB9zi3YIO388d1Q/exec";
 
@@ -107,7 +107,7 @@ function parseDateString(dateStr) {
 }
 
 // ================= TOAST NOTIFICATION =================
-function showToast(message, duration = 3000) {
+function showToast(message, duration = 20000) {
   const toast = document.getElementById("toast");
   toast.innerHTML = message.replace(/\n/g, "<br>");
   toast.classList.add("show");
