274e7fc07035b94ff6dd5e0931457472523f503e
Moon-gazing Cat <nittsdn@gmail.com>
2026-02-27 15:32:26 +0700

    1532 tt

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿// Version: v3.2.1525
+﻿// Version: v3.2.1532
 // ================= CONSTANTS =================
 const API_URL = "https://script.google.com/macros/s/AKfycbzjor1H_-TcN6hDtV2_P4yhSyi46zpoHZsy2WIaT-hJfoZbC0ircbB9zi3YIO388d1Q/exec";
 
@@ -2102,13 +2102,13 @@ window.onload = async () => {
   renderChiChips();
   renderThuChips();
   
+  initHeaderToggle();
   updateHeader(chiData, thuData);
   
   populateChiDropdowns();
   populateThuDropdowns();
   
   initModalEventListeners();
-  initHeaderToggle();
   initTabBar();
 
   console.log('✅ App initialized successfully');
