476423ed1b7ad46b2e2a01136a03071ae8866f70
Moon-gazing Cat <nittsdn@gmail.com>
2026-02-27 19:37:38 +0700

    1937 tt

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿// Version: v3.2.1931
+﻿// Version: v3.2.1937
 // ================= CONSTANTS =================
 const API_URL = "https://script.google.com/macros/s/AKfycbzjor1H_-TcN6hDtV2_P4yhSyi46zpoHZsy2WIaT-hJfoZbC0ircbB9zi3YIO388d1Q/exec";
 
