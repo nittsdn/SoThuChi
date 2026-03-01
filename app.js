f84302eb35a28a43974957fd5f18284f92aaabc7
Moon-gazing Cat <nittsdn@gmail.com>
2026-03-01 09:34:12 +0700

    0933 tt

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿// Version: v3.3.0926
+﻿// Version: v3.3.0933
 // ================= CONSTANTS =================
 const API_URL = "https://script.google.com/macros/s/AKfycbzjor1H_-TcN6hDtV2_P4yhSyi46zpoHZsy2WIaT-hJfoZbC0ircbB9zi3YIO388d1Q/exec";
 
@@ -1281,6 +1281,8 @@ function renderChiChuaTK() {
     rowEl.style.marginBottom = "4px";
     if (isEdited) {
       rowEl.style.borderBottom = "2px dashed #2196f3";
+      rowEl.style.borderTop = "2px dashed #2196f3";
+      rowEl.style.borderRight = "2px dashed #2196f3";
       rowEl.style.borderRadius = "8px";
     }
 
@@ -1403,6 +1405,8 @@ function renderThuChuaTK() {
     rowEl.style.marginBottom = "4px";
     if (isEdited) {
       rowEl.style.borderBottom = "2px dashed #2196f3";
+      rowEl.style.borderTop = "2px dashed #2196f3";
+      rowEl.style.borderRight = "2px dashed #2196f3";
       rowEl.style.borderRadius = "8px";
     }
 
